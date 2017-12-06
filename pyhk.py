6aac3ed1028506f248697f7836346292fe1176e5
FunkMastaZq <rzim1994@gmail.com>
2017-12-06 11:22:40 -0500

    This is now working!

--- pyhk.py
+++ pyhk.py
@@ -43,9 +43,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-import ctypes  # thread, pythoncom, pyHook,
-import PyHook3
-import pythoncom
+import ctypes, PyHook3, pythoncom  #thread
 
 
 class pyhk:
--- testing.py
+++ testing.py
@@ -2,15 +2,13 @@ import pyhk
 
 
 def fun():
-    print
-    "Do something"
-
+    print("Do something")
 
 # create pyhk class instance
 hot = pyhk.pyhk()
 
 # add hotkey
-id1 = hot.addHotkey(['mouse left'], fun)
+id1 = hot.addHotkey(['Ctrl', 'Alt', '7'], fun)
 
 # start looking for hotkey.
 hot.start()
